67d0d7a090c74f135f3325b42f5a632bee767fa0
Matias Espina <matias@devbite.de>
2024-02-07 21:45:56 +0200

    Login screen UI ready

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:expatrio_challenge/services/authentication_service.dart';
+import 'package:expatrio_challenge/theme/expatrio_theme.dart';
 
 import 'package:flutter/material.dart';
 
@@ -16,37 +17,112 @@ class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _passwordController = TextEditingController();
   final storage = const FlutterSecureStorage();
   final AuthenticationService _authService = AuthenticationService();
+  bool _isPasswordVisible = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              controller: _emailController,
-              decoration: const InputDecoration(labelText: 'Email'),
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Center(
+          child: SizedBox(
+            width: 300,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(
+                  height: 50,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 35.0),
+                  child: Center(
+                    child: Image.asset(
+                      'assets/images/logo/ExpatrioLogoBlack_LogoBlack.png',
+                      width: 230,
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(bottom: 8.0),
+                  child: Row(
+                    children: [
+                      Icon(Icons.email_outlined),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        'EMAIL ADDRESS',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+                TextField(
+                  controller: _emailController,
+                  decoration: const InputDecoration(
+                    hintText: 'Email',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+                const Padding(
+                  padding: EdgeInsets.only(bottom: 8.0),
+                  child: Row(
+                    children: [
+                      Icon(Icons.lock_outline),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        'PASSWORD',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+                TextField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    border: const OutlineInputBorder(),
+                    // Agrega el ícono de ojo
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _isPasswordVisible
+                            ? Icons.visibility_off
+                            : Icons.visibility,
+                        color: ExpatrioTheme.primaryColor,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _isPasswordVisible = !_isPasswordVisible;
+                        });
+                      },
+                    ),
+                  ),
+                  obscureText: !_isPasswordVisible,
+                ),
+                const SizedBox(height: 25.0),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(double.infinity, 50),
+                  ),
+                  onPressed: () {
+                    _authService.login(
+                      context: context,
+                      emailController: _emailController,
+                      passwordController: _passwordController,
+                    );
+                  },
+                  child: const Text('LOGIN'),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height - 450,
+                ),
+              ],
             ),
-            TextField(
-              controller: _passwordController,
-              decoration: const InputDecoration(labelText: 'Password'),
-              obscureText: true,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                _authService.login(
-                  context: context,
-                  emailController: _emailController,
-                  passwordController: _passwordController,
-                );
-              },
-              child: const Text('LOGIN'),
-            ),
-          ],
+          ),
         ),
       ),
     );
