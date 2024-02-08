3209e72c5b0eb41baf283901aa3021e6d8b97d74
Matias Espina <matias@devbite.de>
2024-02-08 16:54:42 +0200

    Country picker ready

---
+++ lib/widgets/country_picker_modal_content.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+import '../data/countries_list.dart';
+
+class CountryPickerModal extends StatefulWidget {
+  final Function(String) onCountrySelected;
+  const CountryPickerModal({super.key, required this.onCountrySelected});
+
+  @override
+  CountryPickerModalState createState() => CountryPickerModalState();
+}
+
+class CountryPickerModalState extends State<CountryPickerModal> {
+  List<String> countries = [...countriesList];
+  List<String> filteredCountries = [];
+  String searchQuery = '';
+
+  @override
+  void initState() {
+    super.initState();
+    filteredCountries = countries;
+  }
+
+  void _filterCountries(String query) {
+    setState(() {
+      searchQuery = query;
+      filteredCountries = countries
+          .where((country) =>
+              country.toLowerCase().contains(searchQuery.toLowerCase()))
+          .toList();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DraggableScrollableSheet(
+      expand: false,
+      builder: (_, controller) {
+        return Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(8),
+              child: TextField(
+                onChanged: _filterCountries,
+                decoration: const InputDecoration(
+                  labelText: 'Search',
+                  suffixIcon: Icon(Icons.search),
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                controller: controller,
+                itemCount:
+                    filteredCountries.isEmpty ? 1 : filteredCountries.length,
+                itemBuilder: (context, index) {
+                  if (filteredCountries.isEmpty) {
+                    return const ListTile(
+                        title: Text('No coincidences found...'));
+                  }
+                  return ListTile(
+                    title: Text(filteredCountries[index]),
+                    onTap: () {
+                      widget.onCountrySelected(filteredCountries[index]);
+                      Navigator.pop(context);
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
--- lib/widgets/tax_data_modal_content.dart
+++ lib/widgets/tax_data_modal_content.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'buttons.dart';
+import '../data/countries_list.dart';
+import 'country_picker_modal_content.dart';
 
 class TaxDataModalContent extends StatefulWidget {
   final VoidCallback onTapConfirm;
@@ -12,6 +13,25 @@ class TaxDataModalContent extends StatefulWidget {
 }
 
 class TaxDataModalContentState extends State<TaxDataModalContent> {
+  String selectedCountry = 'Choose a country...';
+
+  void _showCountryPicker() async {
+    String? result;
+    await showModalBottomSheet<String>(
+      context: context,
+      isScrollControlled: true,
+      builder: (BuildContext context) => CountryPickerModal(
+        onCountrySelected: (countrySelected) {
+          result = countrySelected;
+        },
+      ),
+    );
+
+    if (result != null) {
+      setState(() => selectedCountry = result!);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -26,17 +46,29 @@ class TaxDataModalContentState extends State<TaxDataModalContent> {
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
               const Text('Declaration of financial information',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  )),
-              const SizedBox(height: 100),
-              ExpatrioButton(
-                onPressed: () {
-                  widget.onTapConfirm();
-                },
-                text: 'Got it',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              const SizedBox(height: 30),
+              // Resto del contenido
+              Text('Which country serves as your primary tax residence?*'),
+              InkWell(
+                onTap: _showCountryPicker,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(selectedCountry),
+                      Icon(Icons.arrow_drop_down),
+                    ],
+                  ),
+                ),
               ),
+              const SizedBox(height: 8),
+              // Continúa con los otros elementos como el número de identificación fiscal
             ],
           ),
         ),
